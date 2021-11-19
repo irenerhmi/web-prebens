7853cdffe64c78c2ff477897aec585ca381305be
irenerhmi <irenerhmi@student.telkomuniversity.ac.id>
2021-11-19 07:32:47 +0700

    mengganti judul web

--- user/acc-profile.php
+++ user/acc-profile.php
@@ -9,7 +9,7 @@ if(!isset($_SESSION['username']) and $_SESSION['grup'] == 2){
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Prebens Profile </title>
+    <title>Prebens Profilee </title>
 
     <!-- ::::::::::::::Favicon icon::::::::::::::-->
     <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/png">
